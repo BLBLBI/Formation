9a8628a3866ad7c0cb89bb7a6f8ee1c87e55daa3
Killian <gillmann.killian@gmail.com>
2022-03-22 11:43:33 +0100

    search

--- Serie II/Ex1_Search.cs
+++ Serie II/Ex1_Search.cs
@@ -10,14 +10,40 @@ namespace Serie_II
     {
         public static int LinearSearch(int[] tableau, int valeur)
         {
-            //TODO
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                if (tableau[i] == valeur)
+                    return i;
+            }
             return -1;
         }
 
         public static int BinarySearch(int[] tableau, int valeur)
         {
-            //TODO
-            return -1;
+            int res = -1;
+            int g = 0;
+            int d = tableau.Length - 1;
+            int m = d / 2;
+            int i = 0;
+
+            while (res != valeur && i < tableau.Length)
+            {
+                if (tableau[m] < valeur)
+                {
+                    g = m;
+                    m = d - g / 2;
+                }
+                else if (tableau[m] > valeur)
+                {
+                    d = m;
+                    m = d - g / 2;
+                }
+                else
+                    res = tableau[m];
+
+                i++;
+            }
+            return res;
         }
     }
 }
--- Serie II/Program.cs
+++ Serie II/Program.cs
@@ -10,6 +10,15 @@ namespace Serie_II
     {
         static void Main(string[] args)
         {
+            int[] t = new int[] { 1, -5, 10, -3, 0, 4, 2, -7 };
+            int[] td = new int[] { -7, -5, -3, 0, 1, 2, 4, 10 };
+
+            Console.WriteLine(Search.LinearSearch(t, 2));
+            Console.WriteLine(Search.LinearSearch(t, -8));
+
+            Console.WriteLine(Search.BinarySearch(td, 2));
+            Console.WriteLine(Search.BinarySearch(td, -8));
+
 
 
             // Keep the console window open
